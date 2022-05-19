2dd1b96c2df74989bf43e7fb05bfcf10904d507e
Shen Tianqi <1055862487@qq.com>
2022-05-19 21:55:06 +0800

    bool to str

--- report.py
+++ report.py
@@ -58,7 +58,7 @@ def apply(curr_location, logger, auth: NjuUiaAuth, covidTestMethod='YESTERDAY', 
 
         url = URL_JKDK_APPLY + '?' + urlencode(param)
 
-        logger.info(has_applied + "  " + force)
+        logger.info(str(has_applied) + "  " + str(force))
 
         if (not has_applied) or force:
             logger.info('正在打卡')
